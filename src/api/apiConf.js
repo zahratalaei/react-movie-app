eebc946ba79fb6c5d22b5a2c219c3047debeba87
zahratalaei <52117585+zahratalaei@users.noreply.github.com>
2022-11-11 15:16:43 +1000

    Update apiConf.js

--- src/api/apiConf.js
+++ src/api/apiConf.js
@@ -1,6 +1,6 @@
 const apiConf = {
     baseUrl : "https://api.themoviedb.org/3/",
-    apiKey: "",
+    apiKey: "2af988655577d05985545caa7786f118",
     searchUrl : "https://api.themoviedb.org/3/search/movie",
     originalImage:(imgPath)=>`https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image:(imgPath) => `https://image.tmdb.org/t/p/w500${imgPath}`,
